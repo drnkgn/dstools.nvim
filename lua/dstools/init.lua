7d74ee31b51cde7ab5b7cd82eaccdddd810bae2a
drkkgn <jameswong9562@gmail.com>
2023-05-01 12:08:46 +0800

    Call init_cache() in setup to initialize cache.

--- lua/dstools/init.lua
+++ lua/dstools/init.lua
@@ -1,9 +1,7 @@
-local cache = require("dstools.cache")
-local util = require("dstools.util")
-
 local M = {}
 
 function M.setup()
+    require("dstools.cache").init_cache()
     require("dstools.commands").create_commands()
 end
 
